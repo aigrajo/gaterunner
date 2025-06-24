f2d32aa392e853d50eac7e68892f5149e7011b2a
aigrajo <aegrajo@gmail.com>
2025-06-24 08:49:04 -0500

    Update auto_gatekey.sh

--- auto_gatekey.sh
+++ auto_gatekey.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-input="live_urls.txt" # List of urls
+input=$1 # List of urls
+
+total=$(wc -l < "$input")
+count=0
+SECONDS=0
 
 while read -r raw_url; do
+    ((count++))
     # Deobfuscate URL: convert [.] to . and [:] to :
     url=$(echo "$raw_url" | sed 's/\[\.\]/./g; s/\[\:\]/:/g')
 
+    percent=$(( 100 * count / total ))
+    elapsed=$(printf "%02dm:%02ds" $((SECONDS/60)) $((SECONDS%60)))
+
     echo "[*] Running gatekey for: $url"
+    echo "[*] Progress: $count/$total ($percent%) | Elapsed: $elapsed"
 
     # Config
-    python main.py "$url" \
+     python main.py "$url" \
         --country US \
         --ua "Windows;;Chrome" \
         --lang "en-US"
 
-    sleep 1
 done < "$input"
 
+elapsed=$(printf "%02dm:%02ds" $((SECONDS/60)) $((SECONDS%60)))
+echo "[*] Done. Total time: $elapsed"
